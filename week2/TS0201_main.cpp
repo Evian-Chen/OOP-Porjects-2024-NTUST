21ff9d2e076695f4daad1841b61fffb716e32d84
Yan-he Chen <153272499+Evian-Chen@users.noreply.github.com>
2024-03-28 17:53:20 +0800

    Add files via upload

---
+++ week2/TS0201_main.cpp
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * File: TS0201_main.cpp
+ * Author: Evian Chen
+ * Create Date: 2024-03-01
+ * Editor: Evian
+ * Update Date: 2024-03-01
+ * Description: This program caculate the average and standard deviation
+ *				of all input scores.
+***********************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <numeric>
+#include <cmath>
+
+int main()
+{
+	// declare necessary variables
+	std::vector<double> scores;
+	int n;
+	double avg = 0.0, sd = 0.0, total = 0.0, score;
+
+	// infinite loop until cin meets EOF
+	while (std::cin >> n)
+	{
+		// get n scores
+		for (int i = 0; i < n; i++)
+		{
+			// get score and add it to the vecter
+			std::cin >> score;
+			scores.push_back(score);
+		}
+
+		// caculate total and average
+		total = std::accumulate(scores.begin(), scores.end(), 0.0);
+		avg = total / n;
+		
+		// declare a total for caculating standard deviation
+		double sdTotal = 0.0;
+		
+		// loop through scores and add them using the formula
+		for (int i = 0; i < n; i++)
+		{
+			sdTotal += pow((scores[i] - avg), 2);
+		}
+
+		// caculate standard drviation
+		sd = sqrt(sdTotal / n);
+
+		// print out the result
+		std::cout << "Average:" << avg << '\t' << "Standard deviation:" << sd << std::endl;
+		
+		// initialize the vecter
+		scores.clear();
+	}
+}
