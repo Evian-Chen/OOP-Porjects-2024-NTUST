53da60e94e3d4d4def3147ecab64162d406ae48d
Yan-he Chen <153272499+Evian-Chen@users.noreply.github.com>
2024-03-28 17:41:18 +0800

    Update TS0102.cpp

--- week1/TS0102.cpp
+++ week1/TS0102.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-//#include <D:\boost_1_66_0\boost\multiprecision/cpp_int.hpp>
-/*
-This program connot process 18446544175407471631, it will exit instead.
-18446544175407471631 should be a 64-bit integer, but how come it connot be excuted?
-*/
 
 int main() {
-    //boost::multiprecision::cpp_int sec, minute, hour, left_s, left_m;
     long long sec, minute, hour, left_s, left_m;
     while (std::cin >> sec) {
         minute = sec / 60;
