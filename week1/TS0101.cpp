4953522add64c089e57efb6f71f10576f5d3d2a0
Yan-he Chen <153272499+Evian-Chen@users.noreply.github.com>
2024-03-28 17:35:03 +0800

    Update TS0101.cpp

--- week1/TS0101.cpp
+++ week1/TS0101.cpp
@@ -9,7 +9,6 @@ int main() {
 		r = n / guess;
 		double new_guess = (guess + r) / 2;
 		while (guess - new_guess > 0.01) {
-			//std::cout << std::setprecision(4) << new_guess << std::endl;
 			guess = new_guess;
 			r = n / new_guess;
 			new_guess = (new_guess + r) / 2;
